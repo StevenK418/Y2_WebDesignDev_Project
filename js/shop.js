402ee2edaf93637c9607853c60c9d38e2119d49c
StevenK418 <50826648+StevenK418@users.noreply.github.com>
2021-12-16 00:34:44 +0000

    => Added new javascript file to prepopulate contact form fields with existing localstorage data. => Added shop script to all pages to ensure shopping basket is updated on each page load to data saved. => Updated the shop script to ensure the cart quantity is updated on page load.

--- js/shop.js
+++ js/shop.js
@@ -1,4 +1,4 @@
-// shop.js
+// Store management script
 var cartTotal = localStorage.getItem('checkout');
 
 var additemButtons = document.querySelectorAll('.addtocart');
@@ -8,11 +8,13 @@ additemButtons.forEach(button => {
     button.addEventListener("click", addToCart);
 });
 
+//Update the shopping cart quantity to that stored in localstorage
+document.querySelector('.cart-quantity').innerHTML = cartTotal;
 
+//Get a reference to the clear storage link in footer
 var clearBtn = document.querySelector('.clearStorage');
+//Subscribe to click events on this link
 clearBtn.addEventListener('click', ClearCart, false);
-
-// FetchGuitarData();
 
 //Adds an item quantity to the cart
 function addToCart() 
@@ -23,65 +25,9 @@ function addToCart()
     document.querySelector('.cart-quantity').innerHTML=total;
 }
 
-
 //Clears the cart quantity from local storage and resets the counter to 0
 function ClearCart()
 {
-    localStorage.clear();
     localStorage.setItem('checkout',0);
-    document.querySelector('.cart-quantity').innerHTML=0;
+    document.querySelector('.cart-quantity').innerHTML = cartTotal;
 }
-
-
-
-// //Store JSON Populator code
-// function FetchGuitarData()
-// {
-//     let baseUrl = "../res/JSON/guitars.json";
-
-//     //Perform the fetch on the url
-//     fetch(baseUrl)
-//     .then(response=> response.json())
-//     .then(json =>
-//     {
-//         console.log(json);
-
-//         CreateNewStoreItem(json);
-//     })
-//     .catch((error) => {
-//        //Print error message to console
-//         console.log(error);
-//     });
-// }
-
-// function CreateNewStoreItem(json)
-// {
-//     let item = document.createElement('div');
-//     let image = document.createElement('img');
-//     let body = document.createElement('div');
-//     let title = document.createElement('h5');
-
-//     let text = document.createElement('p');
-
-//     let button = document.createElement('a');
-
-
-//     image.src = json.image;
-
-//     title.innerHTML = json.name;
-
-//     text.innerHTML = json.Wood;
-
-//     item.classList.add('card');
-
-//     button.classList.add('btn');
-
-
-//     item.appendChild(image);
-//     item.appendChild(body);
-//     item.appendChild(title);
-//     item.appendChild(text);
-//     item.appendChild(button);
-
-//     document.querySelector('.store-items').appendChild(item);
-// }
--- js/userdetails.js
+++ js/userdetails.js
@@ -3,7 +3,7 @@
 //Data can be viewed and updated via register.html
 if (localStorage.getItem('userdetails') === null) 
 {  
-    var userDetails = {firstName:"Walter", lastName:"Mitty", dob:"1990-12-01",address1:"Buenos Ayres Drive", address2:"Strandhill", address3:"Co. Sligo"};
+    var userDetails = {firstName:"Walter", lastName:"Mitty", dob:"1990-12-01",address1:"Buenos Ayres Drive", address2:"Strandhill", address3:"Co. Sligo", email:"wmitty@email.com", phone: "0451234567"};
    
     localStorage.setItem('userdetails',JSON.stringify(userDetails));
 
